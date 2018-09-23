169d24bdfdb789a5f8b6ad7b69f11419fa98b80a
Yu <weekiiyu@gmail.com>
2018-09-23 12:47:35 +0800

    测试一下

--- App/Http/Routes/default.php
+++ App/Http/Routes/default.php
@@ -1,7 +1,7 @@
 <?php
 
 \Weekii\Core\Http\RouteRule::get('/', function (\Weekii\Core\Http\Request $request, \Weekii\Core\Http\Response $response) {
-    $response->write("<h1>Hello {$request->get('name')}!</h1>");
+    $response->redirect('/hello/' . $request->get('name'));
 });
 
 \Weekii\Core\Http\RouteRule::get('/hello/:name', \App\Http\Controller\IndexController::class . '@hello');
