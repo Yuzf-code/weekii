447fd19f84c5737d4893e28dd797f787654ea206
Yu <619565627@qq.com>
2018-09-24 15:12:49 +0800

    no message

--- App/Http/Controller/IndexController.php
+++ App/Http/Controller/IndexController.php
@@ -8,7 +8,6 @@ class IndexController extends Controller
     public function hello()
     {
         $name = $this->request()->get('name');
-        //$this->write("<h1>Hello! {$name}</h1>");
-        $this->response()->getSwooleResponse()->end("<h1>Hello! {$name}</h1>");
+        $this->write("<h1>Hello! {$name}</h1>");
     }
 }
