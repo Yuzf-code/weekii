ba9ebc26bf032567acb889bfd7bbe304ac06cf3d
Yu <619565627@qq.com>
2019-05-04 01:21:49 +0800

    Model

---
+++ App/Model/Member.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weeki
+ * Date: 2019/5/4
+ * Time: 0:12
+ */
+
+namespace App\Model;
+
+
+class Member
+{
+
+}
---
+++ Weekii/Lib/Database/Model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weeki
+ * Date: 2019/5/3
+ * Time: 21:04
+ */
+
+namespace Weekii\Lib\Database;
+
+
+class Model
+{
+
+}
